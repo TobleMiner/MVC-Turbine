8e41c3264bb5febfeda503ece1e2bfd897af3643
Tobias Schramm <tobleminer@gmail.com>
2016-03-01 17:50:09 +0100

    Explain physics (What is this even?)

--- src/de/mvcturbine/world/physics/PhysicsModel.java
+++ src/de/mvcturbine/world/physics/PhysicsModel.java
@@ -2,14 +2,31 @@ package de.mvcturbine.world.physics;
 
 import de.mvcturbine.world.entity.Entity;
 
+/**
+ * Base of every physics model
+ * 
+ * @author tsys
+ *
+ */
 public abstract class PhysicsModel
 {
+	/** Entity associated with this model */
 	protected final Entity entity;
 
+	/**
+	 * Creates a new physics model for the Entity {@code e}
+	 * 
+	 * @param e
+	 *            The entity
+	 */
 	public PhysicsModel(Entity e)
 	{
 		this.entity = e;
 	}
 
+	/**
+	 * Performs all necessary alteration to the entity to update its state
+	 * according to the physics of this model (e.g. changes velocity)
+	 */
 	public abstract void apply();
 }
